cf1485276ba1fc8879d33993d6074c036e65b68d
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-04-09 03:50:22 +0000

    Update rss.py

--- rss.py
+++ rss.py
@@ -3,7 +3,7 @@ import json
 import feedgen.feed
 
 imageCdn = 'https://images.weserv.nl/?n=-1&url='
-faviconUrl = f'{ imageCdn }https://external-content.duckduckgo.com/ip3/weibo.com.ico'
+faviconUrl = f'{ imageCdn }https://external-content.duckduckgo.com/ip3/www.taiwanplus.com.ico'
 
 # 從 TaiwanPlus 獲取 JSON 數據
 response = niquests.get( 'https://www.taiwanplus.com/api/cms/latestnews' )
@@ -15,23 +15,18 @@ feed.id( 'https://www.taiwanplus.com' )
 feed.title( 'TaiwanPlus - Latest News' )
 feed.description( f'Your source of news, culture, and infotainment from Taiwan, a voice of freedom in Asia.' )
 feed.link( href='https://www.taiwanplus.com', rel='alternate' )
-# feed.icon( f'{ imageCdn }https://external-content.duckduckgo.com/ip3/www.taiwanplus.com.ico' )
 feed.logo( faviconUrl )
 
 # 遍歷 JSON 數據並將條目添加到 feed 中
 for item in data:
-    thumbnailUrl = f'{ imageCdn }{ item['image'] }'
-    
+    thumbnailUrl = f"{ imageCdn }{ item['image'] }"
     feedDescription = f'<img src={ thumbnailUrl } referrerpolicy=no-referrer><br><br>{ item[ "shortDescription" ] }'
-    
-    # 確保 URL 是完整的
     fullUrl = f'https://www.taiwanplus.com/{ item[ "encodedDefaultCategoryFullPath" ] }/{ item[ "vodId" ] }'
 
     entry = feed.add_entry()
     entry.id( fullUrl )
     entry.title( item[ 'title' ] )
     entry.description( feedDescription )
-    # entry.link(href=fullUrl, rel='alternate')
     entry.link( href = fullUrl )
     entry.published( item[ 'publishTime' ] )
 
