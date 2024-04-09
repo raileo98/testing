5961305b7d7f30ea659432deb28f95c6990ce66e
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-04-09 02:22:06 +0000

    Create rss.py

---
+++ rss.py
@@ -0,0 +1,39 @@
+import niquests
+import json
+import feedgen.feed
+
+imageCdn = 'https://images.weserv.nl/?n=-1&url='
+faviconUrl = f'{ imageCdn }https://external-content.duckduckgo.com/ip3/weibo.com.ico'
+
+# 從 TaiwanPlus 獲取 JSON 數據
+response = niquests.get( 'https://www.taiwanplus.com/api/cms/latestnews' )
+data = response.json()
+
+# 創建一個新的 rss feed
+feed = feedgen.feed.FeedGenerator()
+feed.id( 'https://www.taiwanplus.com' )
+feed.title( 'TaiwanPlus - Latest News' )
+feed.description( f'Your source of news, culture, and infotainment from Taiwan, a voice of freedom in Asia.' )
+feed.link( href='https://www.taiwanplus.com', rel='alternate' )
+# feed.icon( f'{ imageCdn }https://external-content.duckduckgo.com/ip3/www.taiwanplus.com.ico' )
+feed.logo( faviconUrl )
+
+# 遍歷 JSON 數據並將條目添加到 feed 中
+for item in data:
+    thumbnailUrl = f'{ imageCdn }{ item['image'] }'
+    
+    feedDescription = f'<img src={ thumbnailUrl } referrerpolicy=no-referrer><br><br>{ item[ "shortDescription" ] }'
+    
+    # 確保 URL 是完整的
+    fullUrl = f'https://www.taiwanplus.com/{ item[ "encodedDefaultCategoryFullPath" ] }/{ item[ "vodId" ] }'
+
+    entry = feed.add_entry()
+    entry.id( fullUrl )
+    entry.title( item[ 'title' ] )
+    entry.description( feedDescription )
+    # entry.link(href=fullUrl, rel='alternate')
+    entry.link( href = fullUrl )
+    entry.published( item[ 'publishTime' ] )
+
+# 將 feed 輸出為 .rss 文件
+feed.rss_file( 'taiwanplus.rss', pretty=True )
