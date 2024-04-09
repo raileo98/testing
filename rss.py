5e472851e5d6c5176785d4799dc7bb4dfebcfa82
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-04-09 05:27:44 +0000

    Update rss.py

--- rss.py
+++ rss.py
@@ -16,11 +16,11 @@ feed = feedgen.feed.FeedGenerator()
 feed.id('https://www.taiwanplus.com')
 feed.title('TaiwanPlus - Latest News')
 feed.description('Your source of news, culture, and infotainment from Taiwan, a voice of freedom in Asia.')
-feed.link(href='https://www.taiwanplus.com')
+feed.link(href='https://www.taiwanplus.com', rel='alternate')
 feed.logo(faviconUrl)
 
 # 遍歷 JSON 數據並將條目添加到 feed 中
-for item in data:
+for item in data['items']:
     thumbnailUrl = f"{imageCdn}{item['image']}"
     feedDescription = f'<img src="{thumbnailUrl}" referrerpolicy="no-referrer"><br><br>{item["shortDescription"]}'
     fullUrl = f'https://www.taiwanplus.com/{item["encodedDefaultCategoryFullPath"]}/{item["vodId"]}'
@@ -29,15 +29,14 @@ for item in data:
     entry.id(fullUrl)
     entry.title(item['title'])
     entry.description(feedDescription)
-    entry.link(href=fullUrl)
+    entry.link(href=fullUrl, rel='alternate')
     entry.published(item['publishTime'])
 
-# 將 feed 輸出為 .rss 文件
-feed.rss_file(outputRssName, pretty=True)
+# 使用 feed.rss_str() 在內存中創建 RSS 字符串
+rss_str = feed.rss_str(pretty=True)
 
-# 解析 .rss 文件並獲取根元素
-tree = ET.parse(outputRssName)
-root = tree.getroot()
+# 使用 ET.fromstring() 從字符串解析 XML
+root = ET.fromstring(rss_str)
 
 # 找到<channel>標籤
 channel = root.find('channel')
@@ -47,5 +46,6 @@ last_build_date = channel.find('lastBuildDate')
 if last_build_date is not None:
     channel.remove(last_build_date)
 
-# 保存修改後的 .rss 文件
-tree.write(outputRssName, encoding='utf-8')
+# 使用 tree.write() 將修改後的 XML 樹直接寫入文件
+tree = ET.ElementTree(root)
+tree.write(outputRssName, encoding='utf-8', xml_declaration=True)
