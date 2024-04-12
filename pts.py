51dfe270a5034cf04f4ffd1e66d12f12f0b0a448
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-04-12 08:53:45 +0000

    Update pts.py

--- pts.py
+++ pts.py
@@ -11,6 +11,29 @@ xml_content = response.text
 # 解析 XML 內容
 root = BeautifulSoup(xml_content, 'xml')
 
+# 遍歷所有的 <channel> 標籤
+channel = root.find('channel')
+
+# 檢查是否存在 <image> 標籤，如果不存在則添加
+if channel.find('image') is None:
+    # 提取 feed domain
+    feed_domain = urllib.parse.urlparse(channel.find('link').text).hostname
+    # 對 feed domain 進行 URL 編碼
+    encoded_feed_domain = urllib.parse.quote_plus(feed_domain)
+    # 創建 <image> 標籤
+    image_tag = root.new_tag('image')
+    url_tag = root.new_tag('url')
+    url_tag.string = f"https://images.weserv.nl/?n=-1&url={urllib.parse.quote_plus('https://external-content.duckduckgo.com/ip3/' + encoded_feed_domain + '.ico')}"
+    image_tag.append(url_tag)
+    title_tag = root.new_tag('title')
+    title_tag.string = channel.find('title').text
+    image_tag.append(title_tag)
+    link_tag = root.new_tag('link')
+    link_tag.string = channel.find('link').text
+    image_tag.append(link_tag)
+    # 將 <image> 標籤添加到 <channel> 標籤中
+    channel.append(image_tag)
+
 # 遍歷所有的 <item> 標籤
 for item in root.find_all('item'):
     # 獲取 <description> 標籤
