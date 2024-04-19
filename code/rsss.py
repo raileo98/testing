0f07dbd4d8cf3f0cca6c080b30b11532682b6119
raileo98 <164594063+raileo98@users.noreply.github.com>
2024-04-19 09:44:01 +0000

    Update rsss.py

--- code/rsss.py
+++ code/rsss.py
@@ -5,7 +5,7 @@ import html
 
 # 創建一個 niquests 會話
 session = niquests.Session(happy_eyeballs=True)
-
+# session = niquests.Session()
 # 設置請求頭部，禁用緩存以獲取最新內容
 session.headers['Cache-Control'] = 'no-cache'
 session.headers['Pragma'] = 'no-cache'
@@ -55,44 +55,64 @@ feeds = {
 
 # 遍歷字典中的每個 RSS 源地址
 for name, url in feeds.items():
-    print(f'url: {url}')
-
+    print( f'url: {url}' )
     # 使用 niquests 發送 HTTP GET 請求到 RSS 源地址
     response = session.get(url)
     # 獲取響應中的 XML 內容
     xml_content = response.text
 
-    # 使用 BeautifulSoup 解析 XML 內容，並使用 lxml 解析器
-    root = BeautifulSoup(xml_content, 'lxml')
+    # 使用 BeautifulSoup 解析 XML 內容
+    root = BeautifulSoup(xml_content, 'xml')
     # 找到 XML 中的 <channel> 標籤
     channel = root.find('channel')
     feed_link = channel.find('link').text
-
+    
     if feed_link:
-        feed_domain = urllib.parse.urlparse(channel.find('link').text).hostname
-        encoded_feed_domain = urllib.parse.quote_plus(feed_domain)
+        if feed_link.startswith('http://') or feed_link.startswith('https://'):
+            feed_domain = urllib.parse.urlparse(channel.find('link').text).hostname
+            encoded_feed_domain = urllib.parse.quote_plus(feed_domain)
+        else:
+            feed_domain = urllib.parse.urlparse( url ).hostname
+            encoded_feed_domain = urllib.parse.quote_plus(feed_domain)
     else:
-        feed_domain = urllib.parse.urlparse(url).hostname
+        feed_domain = urllib.parse.urlparse( url ).hostname
         encoded_feed_domain = urllib.parse.quote_plus(feed_domain)
 
-    # 處理 <image> 標籤
+    # 檢查 <channel> 標籤下是否存在 <image> 標籤
     image_tag = channel.find('image')
-    if image_tag is None:
+    
+    if image_tag is not None:
+        # 如果存在，則替換 <image> 標籤下的 <url> 標籤內容
+        image_tag.find('url').string = CData(f"https://images.weserv.nl/?n=-1&url={urllib.parse.quote_plus('https://external-content.duckduckgo.com/ip3/' + encoded_feed_domain + '.ico')}")
+    
+    else:
+        # 如果不存在 <image> 標籤，則添加一個新的 <image> 標籤
         image_tag = root.new_tag('image')
+        url_tag = root.new_tag('url')
+        # 設置 <url> 標籤的內容
+        url_tag.string = CData(f"https://images.weserv.nl/?n=-1&url={urllib.parse.quote_plus('https://external-content.duckduckgo.com/ip3/' + encoded_feed_domain + '.ico')}")
+        image_tag.append(url_tag)
+        # 添加 <title> 和 <link> 標籤
+        title_tag = root.new_tag('title')
+        title_tag.string = channel.find('title').text
+        image_tag.append(title_tag)
+        link_tag = root.new_tag('link')
+        link_tag.string = channel.find('link').text
+        image_tag.append(link_tag)
+        # 將 <image> 標籤添加到 <channel> 標籤下
         channel.append(image_tag)
-
-    image_tag.find('url', {'url': True}).string = CData(f"https://images.weserv.nl/?n=-1&url={urllib.parse.quote_plus('https://external-content.duckduckgo.com/ip3/' + encoded_feed_domain + '.ico')}")
 
     # 遍歷 <channel> 標籤下的所有 <item> 標籤
     for item in root.find_all('item'):
         # 獲取每個 <item> 標籤下的 <description> 標籤
         description = item.find('description')
+        
         if description is not None:
             # 對 <description> 標籤的內容進行 HTML 實體解碼
             description_text = html.unescape(description.text)
-            # 使用 BeautifulSoup 找到所有的 <img> 標籤，並使用 lxml 解析器
-            img_tags = [img['src'] for img in BeautifulSoup(description_text, 'lxml').find_all('img')]
-
+            # 使用 BeautifulSoup 找到所有的 <img> 標籤
+            img_tags = [img['src'] for img in BeautifulSoup(description_text, 'html.parser').find_all('img')]
+            
             # 遍歷所有的圖片 URL
             for src in img_tags:
                 # 對圖片 URL 進行 URL 編碼
@@ -100,7 +120,6 @@ for name, url in feeds.items():
                 # 替換原始的圖片 URL 為使用 images.weserv.nl 服務的 URL
                 new_src = f"https://images.weserv.nl/?n=-1&url={encoded_image_url}"
                 description_text = description_text.replace(src, new_src)
-
             # 清空原始的 <description> 標籤內容
             description.string = ''
             # 使用 CDATA 包裹更新後的描述內容，以保持 HTML 標籤不被解析
@@ -109,7 +128,7 @@ for name, url in feeds.items():
     # 將修改後的 XML 內容寫入到對應的 RSS 文件中
     with open(f'{name}.rss', 'w', encoding='utf-8') as file:
         file.write(root.prettify())
-    print(f'url: {url} done!')
-
+    print( f'url: {url} done!' )
+    
 # 輸出完成信息
 print('所有 RSS 源已經輸出到對應的文件中。')
